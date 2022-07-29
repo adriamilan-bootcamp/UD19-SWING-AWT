73f0d4c7d0541acee8eaba522975274c4cc99490
FelipeGomezGarcia <feli.goga@gmail.com>
2022-07-29 10:49:20 +0200

    Comentarios ejercicio 3

--- UD1902/src/PeliculasGrafica.java
+++ UD1902/src/PeliculasGrafica.java
@@ -13,39 +13,58 @@ public class PeliculasGrafica extends JFrame {
 	private JPanel contentPane;
 	
 	public PeliculasGrafica() {
+		
+		//Creamos el titulo de la ventana
 		setTitle("Peliculas");
+		
+		//Asignamos la accion de terminar el programa al cerrar la ventana
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//Colocamos la ubicacion y el tamaño de la ventana
 		setBounds(700, 200, 549, 206);
+		
+		//Hacemos visible la ventana
 		setVisible(true);
+		
+		//Creamos el JPanel
 		contentPane = new JPanel();
 		contentPane.setLayout(null);
 		setContentPane(contentPane);
 		
+		//Vamos creando los diferentes componentes de la ventana
+		//Label para la pelicula
 		JLabel tituloLabel = new JLabel("Escribe el titulo de una pelicula");
 		tituloLabel.setBounds(29, 40, 199, 20);
 		contentPane.add(tituloLabel);
 		
+		//TextField para escribir el titulo de la pelicula
 		JTextField tituloPelicula = new JTextField();
 		tituloPelicula.setBounds(39, 71, 127, 20);
 		contentPane.add(tituloPelicula);
 		tituloPelicula.setColumns(10);
 		
+		//Añadimos el comboBox para guardar los titulos
 		JComboBox comboBoxPelis = new JComboBox();
 		comboBoxPelis.setBounds(288, 84, 150, 22);
 		contentPane.add(comboBoxPelis);
 		
+		//Label de peliculas
 		JLabel peliculaLabel = new JLabel("Peliculas");
 		peliculaLabel.setBounds(339, 43, 60, 14);
 		contentPane.add(peliculaLabel);
 		
+		//Añadimos el boton para añadir peliculas al comboBox
 		JButton buttonAdd = new JButton("Añadir");
 		buttonAdd.setBounds(59, 102, 89, 23);
 		
+		//Creamos la accion que hara el boton y lo añadimos al boton
 		buttonAdd.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				//Extraemos el texto del TextField y lo añadimos al ComboBox
 				comboBoxPelis.addItem(tituloPelicula.getText());
+				//Seteamos el text a una cadena vacia
 				tituloPelicula.setText("");
 				
 			}
--- UD1902/src/PeliculasMainApp.java
+++ UD1902/src/PeliculasMainApp.java
@@ -3,6 +3,7 @@ public class PeliculasMainApp {
 	
 	public static void main (String[] args) {
 		
+		//Llamamos a la aplicacion grafica
 		PeliculasGrafica peliculas = new PeliculasGrafica();
 		
 	}
